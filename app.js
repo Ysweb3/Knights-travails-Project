4ea8defb1d0f0e1d37dcbedd64b010dfe4a50b84
yash <yssh88972@gmail.com>
2025-10-06 22:49:42 +0530

    Fix isValid function and implement BFS for knightMoves

--- app.js
+++ app.js
@@ -5,9 +5,32 @@ function knightMoves(start, end){
     [1, -2], [2, -1]
   ];
 
-  function isVaild(){
+  function isValid(x,y){
     return x>=0 && x<8 && y>=0 && x<8;
   }
 
   let queue = [[start,[end]]]
+  let visited = new Set([start.toString()]); 
+
+  while (queue.length > 0) {
+    let [current, path] = queue.shift(); // get first item and removes the first element
+    let [x, y] = current;//sets the current as a simple array
+
+    for (let [dx, dy] of moves) {
+      let next = [x + dx, y + dy];
+      if (isValid(next[0], next[1]) && !visited.has(next.toString())) {
+        visited.add(next.toString());
+        queue.push([next, [...path, next]]);
+      }
+    }
+    //simple if statement to check the pos
+    if (x === end[0] && y === end[1]) {
+      console.log(`You made it in ${path.length - 1} moves!`);
+      path.forEach(p => console.log(p));
+      return path;
+    }
+
+  }
 }
+
+knightMoves([0,0],[5,6])
